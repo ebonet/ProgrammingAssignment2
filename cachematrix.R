becde9fe88b5877e417a85e825fd76b867915c89
Eduardo Bonet <e.f.bonet@gmail.com>
2015-02-08 18:49:19 -0200

    new comments

--- cachematrix.R
+++ cachematrix.R
@@ -15,24 +15,24 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   
   # sets the inverse. Ideally there should be some kind of check here to see 
-  # if the given inv matrix is trully the inverse of matrix x.
-  #In my opinion there 
+  # if the given inv matrix is trully the inverse of matrix x. 
   setInverse <- function(theInverse) inv <<- theInverse 
   
   # returns the inverse
+  # In my opinion there should be no cacheSolve, the check for null should 
+  # happen here, as well as calculating the inverse and caching.
   getInverse <- function() inv
   
+  # returns the list with pointers to the functions
   list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
   
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of a CacheMatrix and returns it
 cacheSolve <- function(x, ...) {
   theInv <- x$getInverse()
   
-  if(is.null(theInv)) {
+  if(is.null(theInv)) { # cache is empty
     message('caching result of inverse') 
     theInv <- solve(x$get(),...) # computes the inverse of the matrix x, passing additional parameters
     x$setInverse(theInv) # sets the inverse to the CacheMatrix
